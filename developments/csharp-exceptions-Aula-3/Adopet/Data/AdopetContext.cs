aea2adad29308780a07a07306415e60818d9858e
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:49 -0300

    feat: adição do arquivo AdopetContext.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Data/AdopetContext.cs
@@ -0,0 +1,30 @@
+﻿using Adopet.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Adopet.Data;
+
+public class AdopetContext : DbContext
+{
+    public AdopetContext(DbContextOptions<AdopetContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Adocao>()
+            .HasOne(a => a.Pet)
+            .WithOne(p => p.Adocao)
+            .HasForeignKey<Adocao>(a => a.PetId);
+
+        modelBuilder.Entity<Adocao>()
+            .HasOne(a => a.Tutor)
+            .WithMany(t => t.Adocoes)
+            .HasForeignKey(a => a.TutorId);
+    }
+
+    public DbSet<Adocao> Adocoes { get; set; }
+    public DbSet<Pet> Pets { get; set; }
+    public DbSet<Tutor> Tutores { get; set; }
+}
