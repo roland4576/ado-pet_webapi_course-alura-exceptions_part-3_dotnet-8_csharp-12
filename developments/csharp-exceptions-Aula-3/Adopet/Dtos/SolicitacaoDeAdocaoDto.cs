6790b8c73819189ffab5d376a98788baae067e16
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:54 -0300

    feat: adição do arquivo SolicitacaoDeAdocaoDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/SolicitacaoDeAdocaoDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adopet.Dtos;
+
+public record SolicitacaoDeAdocaoDto
+{
+    [Required]
+    public long IdPet { get; init; }
+
+    [Required]
+    public long IdTutor { get; init; }
+
+    [Required]
+    public string Motivo { get; init; }
+}
