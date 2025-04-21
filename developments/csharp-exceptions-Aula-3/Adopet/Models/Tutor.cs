7d5c036acff4b62a40feeea016195acd87d63fc8
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:40 -0300

    feat: adição do arquivo Tutor.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Models/Tutor.cs
@@ -0,0 +1,27 @@
+﻿using Adopet.Api.Dto;
+using System.ComponentModel.DataAnnotations;
+
+namespace Adopet.Models;
+
+public class Tutor
+{
+    [Key]
+    [Required]
+    public long Id { get; set; }
+    [Required]
+    public string Nome { get; set; }
+    [Required]
+    public string Email { get; set; }
+
+    public virtual List<Adocao> Adocoes { get; set; }
+
+    public Tutor()
+    {
+    }
+
+    public Tutor(CadastroTutorDto dados)
+    {
+        Nome = dados.Nome;
+        Email = dados.Email;
+    }
+}
