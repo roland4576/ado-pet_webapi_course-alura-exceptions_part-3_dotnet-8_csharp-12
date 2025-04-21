16b5fae54816368bb4771ae520000469b52a4947
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:47 -0300

    feat: adição do arquivo PetRepository.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Repositories/PetRepository.cs
@@ -0,0 +1,31 @@
+﻿using Adopet.Data;
+using Adopet.Models;
+
+namespace Adopet.Repositories;
+
+public class PetRepository
+{
+    private readonly AdopetContext _dbContext;
+
+    public PetRepository(AdopetContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Pet? GetById(long id)
+    {
+        return _dbContext.Pets.FirstOrDefault(p => p.Id == id);
+    }
+
+    public IEnumerable<Pet> GetAll()
+    {
+        return _dbContext.Pets.ToList();
+    }
+
+    public void Add(Pet pet)
+    {
+        _dbContext.Pets.Add(pet);
+        _dbContext.SaveChanges();
+    }
+
+}
