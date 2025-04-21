51a91051726f59c1e7fc28cd3b2c02a1ed1a6d26
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:38 -0300

    feat: adição do arquivo 20240526195834_sqlite init ef.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Migrations/20240526195834_sqlite init ef.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Adopet.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqliteinitef : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pets",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Idade = table.Column<int>(type: "INTEGER", nullable: false),
+                    Tipo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Adotado = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Imagem = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tutores",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tutores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Adocoes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TutorId = table.Column<long>(type: "INTEGER", nullable: false),
+                    PetId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Motivo = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Justificativa = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Adocoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Adocoes_Pets_PetId",
+                        column: x => x.PetId,
+                        principalTable: "Pets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Adocoes_Tutores_TutorId",
+                        column: x => x.TutorId,
+                        principalTable: "Tutores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adocoes_PetId",
+                table: "Adocoes",
+                column: "PetId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Adocoes_TutorId",
+                table: "Adocoes",
+                column: "TutorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Adocoes");
+
+            migrationBuilder.DropTable(
+                name: "Pets");
+
+            migrationBuilder.DropTable(
+                name: "Tutores");
+        }
+    }
+}
