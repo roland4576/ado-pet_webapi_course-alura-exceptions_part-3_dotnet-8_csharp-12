2313928f83262d6f87e0eab0c12a15fb709fcc81
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:42 -0300

    feat: adição do arquivo TutorService.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Services/TutorService.cs
@@ -0,0 +1,25 @@
+﻿using Adopet.Api.Dto;
+using Adopet.Models;
+using Adopet.Repositories;
+
+namespace Adopet.Services;
+
+public class TutorService
+{
+    private readonly TutorRepository _repository;
+
+    public TutorService(TutorRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public List<TutorDto> ListarTodos()
+    {
+        return _repository.GetAll().Select(tutor => new TutorDto(tutor)).ToList();
+    }
+
+    public void Cadastrar(CadastroTutorDto dados)
+    {
+        _repository.Add(new Tutor(dados));
+    }
+}
