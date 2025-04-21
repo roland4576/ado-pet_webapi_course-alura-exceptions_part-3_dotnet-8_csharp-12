68461bb0135b948cfa9b5ce6996306c47a0071d4
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:43 -0300

    feat: adição do arquivo ImageStorageService.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Services/ImageStorageService.cs
@@ -0,0 +1,33 @@
+﻿namespace Adopet.Services;
+
+public class ImageStorageService
+{
+    private readonly string _uploadFolder;
+
+    public ImageStorageService(IWebHostEnvironment webHostEnvironment)
+    {
+        _uploadFolder = Path.Combine(webHostEnvironment.ContentRootPath, "storage");
+    }
+
+    public string Upload(IFormFile imagem)
+    {
+        string novoNome = GerarNovoNome(imagem.FileName);
+
+        string caminhoCompletoDoArquivo = Path.Combine(_uploadFolder, novoNome);
+
+        using (var stream = new FileStream(caminhoCompletoDoArquivo, FileMode.Create))
+        {
+            imagem.CopyTo(stream);
+        }
+
+        return novoNome;
+    }
+
+    private string GerarNovoNome(string nomeOriginal)
+    {
+        string[] nomeSplit = nomeOriginal.Split('.');
+        string extensao = "." + nomeSplit[1];
+
+        return Guid.NewGuid() + extensao;
+    }
+}
