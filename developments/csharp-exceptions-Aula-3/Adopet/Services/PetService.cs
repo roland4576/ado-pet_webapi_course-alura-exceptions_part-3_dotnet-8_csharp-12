b84d157ae49b3d70cbfc6b5880c78c393f933f57
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:44 -0300

    feat: adição do arquivo PetService.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Services/PetService.cs
@@ -0,0 +1,29 @@
+﻿using Adopet.Dtos;
+using Adopet.Models;
+using Adopet.Repositories;
+
+namespace Adopet.Services;
+
+public class PetService
+{
+    private readonly PetRepository _repository;
+    private readonly ImageStorageService _imagemService;
+
+    public PetService(PetRepository repository, ImageStorageService imagemService)
+    {
+        _repository = repository;
+        _imagemService = imagemService;
+    }
+
+    public List<PetDto> ListarTodos()
+    {
+        return _repository.GetAll().Select(pet => new PetDto(pet)).ToList();
+    }
+
+    public void Cadastrar(CadastroPetDto dto)
+    {
+        string nomeImagem = _imagemService.Upload(dto.Imagem);
+
+        _repository.Add(new Pet(dto, nomeImagem));
+    }
+}
