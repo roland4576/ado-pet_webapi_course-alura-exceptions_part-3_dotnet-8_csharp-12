43300844708bdd3ca56938b3df802906cef633b4
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:45 -0300

    feat: adição do arquivo PetController.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Adopet.Dtos;
+using Adopet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adopet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PetController : ControllerBase
+{
+    private readonly PetService _petService;
+
+    public PetController(PetService petService)
+    {
+        _petService = petService;
+    }
+
+    [HttpGet]
+    public IActionResult BuscarTodos()
+    {
+        List<PetDto> pets = _petService.ListarTodos();
+        return Ok(pets);
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar([FromForm] CadastroPetDto dados)
+    {
+            _petService.Cadastrar(dados);
+            return Ok();      
+    }
+}
