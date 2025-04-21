bb2f5494e2c2fad4c76c58bff51f467e5a3d5026
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:47 -0300

    feat: adição do arquivo TutorController.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Controllers/TutorController.cs
@@ -0,0 +1,31 @@
+﻿using Adopet.Api.Dto;
+using Adopet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adopet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TutorController : ControllerBase
+{
+    private readonly TutorService _service;
+
+    public TutorController(TutorService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult BuscarTodos()
+    {
+        List<TutorDto> tutores = _service.ListarTodos();
+        return Ok(tutores);
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar([FromBody] CadastroTutorDto dados)
+    {
+        _service.Cadastrar(dados);
+        return Ok();
+    }
+}
