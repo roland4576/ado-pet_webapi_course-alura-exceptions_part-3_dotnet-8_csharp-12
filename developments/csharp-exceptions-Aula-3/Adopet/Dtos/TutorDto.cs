f09527ec46ac91f9eeda84ee072d26b0c130fd6b
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:56 -0300

    feat: adição do arquivo TutorDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/TutorDto.cs
@@ -0,0 +1,10 @@
+﻿using Adopet.Models;
+
+namespace Adopet.Api.Dto;
+
+public record TutorDto(long Id, string Nome, string Email)
+{
+    public TutorDto(Tutor tutor) : this(tutor.Id, tutor.Nome, tutor.Email)
+    {
+    }
+}
