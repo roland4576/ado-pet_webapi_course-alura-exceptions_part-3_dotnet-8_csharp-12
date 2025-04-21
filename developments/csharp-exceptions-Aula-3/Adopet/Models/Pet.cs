412a6a580dfe6bba1f8013f2edec9a9e9e6fcdcc
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:39 -0300

    feat: adição do arquivo Pet.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Models/Pet.cs
@@ -0,0 +1,37 @@
+﻿
+using Adopet.Dtos;
+using Adopet.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Adopet.Models;
+
+public class Pet
+{
+    [Key]
+    [Required]
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public TipoPet Tipo { get; set; }
+    public bool Adotado { get; set; }
+    public string Imagem { get; set; }
+    public virtual Adocao Adocao { get; set; }
+
+    public Pet()
+    {
+    }
+
+    public Pet(CadastroPetDto dados, string nomeDaImagem)
+    {
+        Nome = dados.Nome;
+        Idade = dados.Idade;
+        Tipo = dados.Tipo;
+        Imagem = nomeDaImagem;
+        Adotado = false;
+    }
+
+    public void MarcarComoAdotado()
+    {
+        Adotado = true;
+    }
+}
