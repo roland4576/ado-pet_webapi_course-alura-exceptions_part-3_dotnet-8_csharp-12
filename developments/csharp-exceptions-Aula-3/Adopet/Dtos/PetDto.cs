228b226b6ef0328efeb76141a9c4c62a4bc921d2
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:52 -0300

    feat: adição do arquivo PetDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/PetDto.cs
@@ -0,0 +1,11 @@
+﻿using Adopet.Models;
+using Adopet.Models.Enums;
+
+namespace Adopet.Dtos;
+
+public record PetDto(long Id, string Nome, int Idade, TipoPet Tipo, bool Adotado, string Imagem)
+{
+    public PetDto(Pet pet) : this(pet.Id, pet.Nome, pet.Idade, pet.Tipo, pet.Adotado, pet.Imagem)
+    {
+    }
+}
