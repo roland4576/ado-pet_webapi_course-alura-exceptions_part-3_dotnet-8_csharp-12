d073441f2c8c37a299abfe70330c22e0cdf1f0c7
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:53 -0300

    feat: adição do arquivo CadastroPetDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/CadastroPetDto.cs
@@ -0,0 +1,17 @@
+﻿
+using Adopet.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Adopet.Dtos;
+
+public record CadastroPetDto
+{
+    [Required]
+    public string Nome { get; set; }
+    [Required]
+    public int Idade { get; set; }
+    [Required]
+    public TipoPet Tipo { get; set; }
+    [Required]
+    public IFormFile Imagem { get; set; }
+}
