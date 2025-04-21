f34d4c07fd90b917faf1e397f93ed36c7c1459c5
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:42 -0300

    feat: adição do arquivo AdocaoService.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Services/AdocaoService.cs
@@ -0,0 +1,75 @@
+﻿using Adopet.Dtos;
+using Adopet.Exceptions;
+using Adopet.Models;
+using Adopet.Models.Enums;
+using Adopet.Repositories;
+
+namespace Adopet.Services;
+
+public class AdocaoService
+{
+    private readonly PetRepository _petRepository;
+    private readonly TutorRepository _tutorRepository;
+    private readonly AdocaoRepository _adocaoRepository;
+    public AdocaoService(PetRepository petRepository, TutorRepository tutorRepository, AdocaoRepository adocaoRepository)
+    {
+        _petRepository = petRepository;
+        _tutorRepository = tutorRepository;
+        _adocaoRepository = adocaoRepository;
+    }
+
+    public List<AdocaoDto> ListarTodos()
+    {
+        return _adocaoRepository.GetAll().Select(adocao => new AdocaoDto(adocao)).ToList();
+    }
+
+    public AdocaoDto? Listar(long id)
+    {
+        var adocao = _adocaoRepository.GetById(id);
+        return adocao != null ? new AdocaoDto(adocao) : null;
+    }
+
+    public void Solicitar(SolicitacaoDeAdocaoDto dto)
+    {
+
+        var pet = _petRepository.GetById(dto.IdPet);
+        var tutor = _tutorRepository.GetById(dto.IdTutor);
+
+        if(pet == null || tutor == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        if (pet.Adotado)
+        {
+            throw new PetAdotadoException("Pet já foi adotado!");
+        }
+        if(_adocaoRepository.ExistsByPetIdAndStatus(pet.Id, StatusAdocao.AGUARDANDO_AVALIACAO))
+        {
+            throw new PetEmProcessoDeAdocaoException("Pet está sob processo de adoção!");
+        }
+        var numeroDeAdocoes = _adocaoRepository.
+               CountByTutorIdAndStatus(tutor.Id, StatusAdocao.APROVADO);
+        if (numeroDeAdocoes >= 2)
+        {
+            throw new TutorComLimiteAtingidoException("Tutor não pode mais adotar!");
+        }
+
+        _adocaoRepository.Add(new Adocao(tutor, pet, dto.Motivo));
+    }
+
+    public void Aprovar(AprovarAdocaoDto dto)
+    {
+        var adocao = _adocaoRepository.GetById(dto.IdAdocao);
+        adocao.MarcarComoAprovada();
+        adocao.Pet.MarcarComoAdotado();
+        _adocaoRepository.SaveChanges();
+    }
+
+    public void Reprovar(ReprovarAdocaoDto dto)
+    {
+        var adocao = _adocaoRepository.GetById(dto.IdAdocao);
+        adocao.MarcarComoReprovada(dto.Justificativa);
+        _adocaoRepository.SaveChanges();
+    }
+}
