b51120785d6bb34948588ea2ded959535f2a1142
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:48 -0300

    feat: adição do arquivo TutorRepository.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Repositories/TutorRepository.cs
@@ -0,0 +1,30 @@
+﻿using Adopet.Data;
+using Adopet.Models;
+
+namespace Adopet.Repositories;
+
+public class TutorRepository
+{
+    private readonly AdopetContext _dbContext;
+
+    public TutorRepository(AdopetContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Tutor? GetById(long id)
+    {
+        return _dbContext.Tutores.FirstOrDefault(t => t.Id == id);
+    }
+
+    public IEnumerable<Tutor> GetAll()
+    {
+        return _dbContext.Tutores.ToList();
+    }
+
+    public void Add(Tutor tutor)
+    {
+        _dbContext.Tutores.Add(tutor);
+        _dbContext.SaveChanges();
+    }
+}
