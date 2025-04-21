a5dab0e19d0f69349aebcd48e581535f7a3b60b4
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:57 -0300

    feat: adição do arquivo CadastroTutorDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/CadastroTutorDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Adopet.Api.Dto;
+public record CadastroTutorDto
+{
+    [Required]
+    public string Nome { get; init; }
+
+    [Required]
+    public string Email { get; init; }
+}
