fe923240772dac3472e38074c65da473c21957a5
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:55 -0300

    feat: adição do arquivo AdocaoDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/AdocaoDto.cs
@@ -0,0 +1,11 @@
+﻿using Adopet.Models;
+using Adopet.Models.Enums;
+
+namespace Adopet.Dtos;
+
+public record AdocaoDto(long Id, long Tutor, long Pet, string Motivo, StatusAdocao Status, string Justificativa)
+{
+    public AdocaoDto(Adocao adocao) : this(adocao.Id, adocao.Tutor.Id, adocao.Pet.Id, adocao.Motivo, adocao.Status, adocao.Justificativa)
+    {
+    }
+}
