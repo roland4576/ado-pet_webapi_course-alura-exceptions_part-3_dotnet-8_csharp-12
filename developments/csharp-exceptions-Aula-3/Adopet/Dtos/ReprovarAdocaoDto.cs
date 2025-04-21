7f2960d7cc298d9ddb03ac101b1157a3618d8334
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-04-21 19:09:57 -0300

    feat: adição do arquivo ReprovarAdocaoDto.cs

---
+++ developments/csharp-exceptions-Aula-3/Adopet/Dtos/ReprovarAdocaoDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Adopet.Dtos;
+
+public record ReprovarAdocaoDto
+{
+    [Required]
+    [JsonPropertyName("idAdocao")]
+    public long IdAdocao { get; init; }
+
+    [Required]
+    [StringLength(int.MaxValue, MinimumLength = 1)]
+    [JsonPropertyName("justificativa")]
+    public string Justificativa { get; init; }
+}
